bd334ae7442f688875f53e572b4e7e07ada1b73d
qiu <qiuxlei@163.com>
2013-05-14 22:18:18 +0800

    First Version

---
+++ src/main/java/netcia/org/cpkf/App.java
@@ -0,0 +1,12 @@
+package netcia.org.cpkf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+    public static void main( String[] args ){
+        HelloWorld hello = new HelloWorld();
+        System.out.println( hello.SayHello());
+    }
+}
---
+++ src/main/java/netcia/org/cpkf/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netcia.org.cpkf;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelloWorld {
+    /**
+     * Get output file object
+     */
+    public String SayHello(){
+        return "HelloWorld";
+    }
+}
